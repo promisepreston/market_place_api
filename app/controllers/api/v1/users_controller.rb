fbd992f31d251262e3c4633ac8e8a1652152b55e
Promise Chukwuenyem <promisepreston@gmail.com>
2020-03-12 06:25:15 +0100

    add the user delete endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: %i[show update destroy]
+
   # GET /users/1
   def show
-    render json: User.find(params[:id])
+    render json: @user_find
   end
 
   # POST /users
@@ -17,11 +19,17 @@ class Api::V1::UsersController < ApplicationController
 
   # PATCH/PUT /users/1
   def update
-    if @user.update(user_params)
+    if @user_find.update(user_params)
       render json: @user, status: :ok
     else
       render json: @user.errors, status: :unprocessable_entity
     end
+  end
+
+  # DELETE /users/1
+  def destroy
+    @user.destroy
+    head 204
   end
 
   private
@@ -32,6 +40,6 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def set_user
-    @user = User.find(params[:id])
+    @user_find = User.find(params[:id])
   end
 end
